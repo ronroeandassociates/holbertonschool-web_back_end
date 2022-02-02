60e15ed33fe369f62aa1a4ccf26701422c9da915
Ronroeandassociates <3321@holbertonschool.com>
2022-02-01 18:09:19 -0600

    semicolon:

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
 export function taskFirst () {
   const task = 'I prefer const when I can.'
-  return task
+  return task;
 }
 
 export function getLast () {
-  return ' is okay'
+  return ' is okay';
 }
 
 export function taskNext () {
   let combination = 'But sometimes let'
-  combination += getLast()
+  combination += getLast();
 
-  return combination
+  return combination;
 }
