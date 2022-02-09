18976e4943c4fb5468143e73e4fb4c6759865678
Ronroeandassociates <3321@holbertonschool.com>
2022-02-08 19:44:36 -0600

    try again

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,6 +1,6 @@
 export default class Car {
   constructor(brand, motor, color) {
-    // Create objs
+    // Create objects
     this._brand = brand;
     this._motor = motor;
     this._color = color;
@@ -9,15 +9,10 @@ export default class Car {
   // Methods
 
   cloneCar() {
-    const NewObj = this.constructor[Symbol.species] || this.constructor;
-    const clone = new NewObj();
-    return clone;
+    return Object.assign(Object.create(Object.getPrototypeOf(this)), {
+      _brand: undefined,
+      _motor: undefined,
+      _color: undefined,
+    });
   }
-
 }
-/* class TestCar extends Car {};
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
-console.log(tc1 instanceof TestCar);
-console.log(tc2 instanceof TestCar);
-console.log(tc1 == tc2); */
