4248b0182f64ef96c120a5f63bd1d8c15ff9fdfa
Ronroeandassociates <3321@holbertonschool.com>
2022-06-01 09:34:34 -0500

    TASK: 3 Basic annotations - to string
    a type-annotated function to_str that takes a float n as argument and returns the string representation of the float

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+3 Basic annotations - to string
+
+a type-annotated function to_str that takes a float n as
+argument and returns the string representation of the float
+
+Function:
+    to_str(n: float) -> str
+"""
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of n.
+    """
+    return str(n)
