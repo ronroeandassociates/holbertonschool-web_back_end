b7b290dcc8397e946db2a0ee75375366eb2ea189
Ronroeandassociates <3321@holbertonschool.com>
2022-06-01 09:16:50 -0500

    TASK: 1 Basic annotations - concat
    a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+"""
+Basic annotations - concat
+a type-annotated function concat that takes a string str1 and a string str2 as
+arguments and returns a concatenated string
+
+Functions:
+  concat(str1: str, str2: str) -> str
+"""
+
+def concat(str1: str,str2: str) -> str:
+    """
+    Return the concatenation of str1 and str2.
+    """
+    return str1 + str2
