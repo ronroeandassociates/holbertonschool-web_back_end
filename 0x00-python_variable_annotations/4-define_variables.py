7d759ac73b8c7b60730ae8886cff026253b79008
Ronroeandassociates <3321@holbertonschool.com>
2022-06-01 12:36:09 -0500

    fixed a typo

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -11,11 +11,7 @@ school, a string with a value of “Holberton”
 
 """
 
-a:
-    int = 1
-pi:
-    float = 3.14
-i_understand_annotations:
-    bool = True
-school:
-    str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
   7 Complex types - string and int/float to tuple
 
 Write a type-annotated function to_kv that takes a string k and
-an int OR float v asarguments and returns a tuple. The first
+an int OR float v as arguments and returns a tuple. The first
 element of the tuple is the string k The second element is the
 square of the int/float v and should be annotated as a float
 
