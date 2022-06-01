d3192001d1f12fab15d4301d7f92894f67a48994
Ronroeandassociates <3321@holbertonschool.com>
2022-06-01 10:17:47 -0500

    TASK: 9 Lets duck type an iterable object
    Annotate the below functions parameters and return values with the appropriate types
    
    def element_length(lst):
        return [(i, len(i)) for i in lst]

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+  9 Let's duck type an iterable object
+
+Annotate the below function’s parameters and return values with the appropriate types
+
+def element_length(lst):
+    return [(i, len(i)) for i in lst]
+
+Functions:
+    def element_length(lst: Iterable) -> List[Tuple[str, int]]:
+"""
+from typing import Iterable,Sequence,List,Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return a list of tuples of the form (element, length).
+    """
+    return [(i, len(i)) for i in lst]
