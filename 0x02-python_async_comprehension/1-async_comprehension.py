4d0dbb6f16e29f3b9bba4d4fa8876fb611222696
Ronroeandassociates <3321@holbertonschool.com>
2022-06-06 12:52:33 -0500

    Task 1 Async Comprehensions
    Import async_generator from the previous task
    
    write a coroutine called async_comprehension that takes no arguments
    
    The coroutine will collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+"""_summary_
+1 Async Comprehensions
+Import async_generator from the previous task
+write a coroutine called async_comprehension that takes no arguments
+The coroutine will collect 10 random numbers using an async
+comprehensing over async_generator, then return the 10 random numbers
+
+Function:
+    async_comprehension() -> list[float]:
+"""
+
+import asyncio
+import random
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    The coroutine will loop 10 times each time asynchronously wait 1 second
+    then yield a random number between 0 and 10
+    Use the random module to generate a random number between 0 and 10
+    """
+    max_delay = 10
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
