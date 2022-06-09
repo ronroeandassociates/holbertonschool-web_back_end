ab8fcdbb840abb7bd52a42da082933d7b738e314
Ronroeandassociates <3321@holbertonschool.com>
2022-06-09 17:49:12 -0500

    [let's put it all back together the way we started and settle for missing one check

--- 0x00-caching/3-lru_cache.py
+++ 0x00-caching/3-lru_cache.py
@@ -34,12 +34,12 @@ class LRUCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """put item into cache_data with LIFO algorithm
+        """put item into cache_data with LRU algorithm
         Args:
             key ([type]): key of dictionary
             item ([type]): item to insert in dictionary
         """
-        if len(self.cache_data) == self.MAX_ITEMS and key not in self:
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
             discard = self.__keys.pop(0)
             del self.cache_data[discard]
             print('DISCARD: {}'.format(discard))
@@ -48,14 +48,3 @@ class LRUCache(BaseCaching):
                 self.__keys.remove(key)
             self.__keys.append(key)
             self.cache_data[key] = item
-
-    def get(self, key):
-        """get value of cache_data dictionary
-        Args:
-            key ([type]): key to search into cache_data
-        """
-        if not key or key not in self.cache_data:
-            return None
-        self.__keys.remove(key)
-        self.__keys.append(key)
-        return self.cache_data[key]
