3a3fbd3888c2e64ab205bb4cb95edb7eb091ebe1
Ronroeandassociates <3321@holbertonschool.com>
2022-06-09 17:20:19 -0500

    [ATTEMPT 9] resolve to get the remaining check

--- 0x00-caching/3-lru_cache.py
+++ 0x00-caching/3-lru_cache.py
@@ -34,22 +34,28 @@ class LRUCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        """put item into cache_data with LIFO algorithm
+        Args:
+            key ([type]): key of dictionary
+            item ([type]): item to insert in dictionary
         """
-        Add an item in the cache
-        """
-        if (key and item):
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            discard = self.__keys.pop(0)
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        if key and item:
+            if key in self.cache_data:
+                self.__keys.remove(key)
+            self.__keys.append(key)
             self.cache_data[key] = item
-            if len(self.cache_data) > self.MAX_ITEMS:
-                key, val = self.cache_data.popitem(last=False)
-                print("DISCARD: {}".format(key))
 
     def get(self, key):
+        """get value of cache_data dictionary
+        Args:
+            key ([type]): key to search into cache_data
         """
-        Get an item from the cache
-        """
-        if key is None:
+        if not key or key not in self.cache_data:
             return None
-        if key in self.cache_data:
-            return self.cache_data[key]
-        else:
-            return None
+        self.__keys.remove(key)
+        self.__keys.append(key)
+        return self.cache_data[key]
