06aab7529cd984edf6e7a5225b0f5f50599faf43
Ronroeandassociates <3321@holbertonschool.com>
2022-06-13 14:30:45 -0500

    [Getting AttibuteError:'Server' object has no attribute 'get_hyper']
    TASK: 2 Hypermedia pagination
    Replicate code from the previous task
    
    Implement a get_hyper method that takes the same arguments (and defaults) as get_page and returns a dictionary containing the following key-value pairs:
    
    page_size: the length of the returned dataset page
    page: the current page number
    data: the dataset page (equivalent to return from previous task)
    next_page: number of the next page, None if no next page
    prev_page: number of the previous page, None if no previous page
    total_pages: the total number of pages in the dataset as an integer
    Make sure to reuse get_page in your implementation
    
    You can use the math module if necessary

--- 0x01-pagination/2-hypermedia_pagination.py
+++ 0x01-pagination/2-hypermedia_pagination.py
@@ -4,7 +4,56 @@ __Summary__
 2 Hypermedia pagination
 Replicate code from the previous task.
 
-Implement a get_hyper method that takes the same arguments (and defaults) as get_page and returns a dictionary containing the following key-value pairs:
+Classes:
+    Server
+
+Functions: tselflowing functions are implemented:
+    dataset(self) -> List[List]
+    get_page(self, int, int) -> List[List]
+    index_range(self, int, int) -> Tuple[int, int]
+    get_hyper(self, int, int) -> dict
+
+"""
+import csv
+import math
+from typing import List, Dict
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return page of the dataset
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        page, page_size = index_range(page, page_size)
+        try:
+            return self.dataset()[page:page_size]
+        except IndexError:
+            return []
+
+
+"""
+Implement a get_hyper method that takes the same arguments (and defaults)
+as get_page and returns a dictionary containing the following key-value pairs:
 
 page_size: the length of the returned dataset page
 page: the current page number
@@ -16,3 +65,22 @@ Make sure to reuse get_page in your implementation.
 
 You can use the math module if necessary.
 """
+
+
+def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+    """Return page of the dataset"""
+
+    listpage = self.get_page(page, page_size)
+
+    total_pages = math.ceil(len(self.dataset()) / page_size)
+
+    prev_page = page - 1 if page > 1 else None
+    next_page = page + 1 if page < total_pages else None
+    page_size = 0 if listpage == [] else page_size
+
+    return {'page_size': len(listpage),
+                  'page': page,
+                  'data': listpage,
+                  'next_page': next_page,
+                  'prev_page': prev_page,
+                  'total_pages': total_pages}
