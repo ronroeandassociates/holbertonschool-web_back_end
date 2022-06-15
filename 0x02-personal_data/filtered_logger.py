7b2bdff771848df798fec41a49a5742e9a79b4d9
Ronroeandassociates <3321@holbertonschool.com>
2022-06-15 13:59:38 -0500

    Implement a get_logger function that takes no arguments and returns a logging.Logger object.
    The logger should be named user_data and only log up to logging.INFO level. It should not propagate messages to other loggers. It should have a StreamHandler with RedactingFormatter as formatter.
    
    Create a tuple PII_FIELDS constant at the root of the module containing the fields from user_data.csv that are considered PII. PII_FIELDS can contain only 5 fields - choose the right list of fields that can are considered as “important” PIIs or information that you must hide in your logs. Use it to parameterize the formatter

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -2,11 +2,61 @@
 """
 0 Regex-ing
 Write a function called filter_datum that returns the log message obfuscated:
-Arguments:
-fields: a list of strings representing all fields to obfuscate
-redaction: a string representing by what the field will be obfuscated
-message: a string representing the log line
-separator: a string representing by which character is separating all fields in the log line (message)
-The function should use a regex to replace occurrences of certain field values.
-filter_datum should be less than 5 lines long and use re.sub to perform the substitution with a single regex
 """
+
+import re
+import logging
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+"""
+1 Log formatter
+"""
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        ''' Initialize the formatter with the fields to redact'''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        ''' Format the log message '''
+        msg = super().format(record)
+        logList =  (filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR))
+        return logList.replace(";", "; ")
+        NotImplementedError
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    fields: list of strings representing all fields to obfuscate
+    redaction: string representing by what the field will be obfuscated
+    message: string representing the log line
+    separator: string representing by which character is separating all
+    fields in the log line (message)
+    return: string representing the log line obfuscated
+    """
+    for fields in fields:
+        regex = fr"(?<={fields}=).*?(?={separator})"
+        message = re.sub(r'{}'.format(regex), redaction, message)
+    return message
+
+def get_logger() -> logging.Logger:
+    """
+    2 Create logger
+    """
+
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    formatter = RedactingFormatter(PII_FIELDS)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.propagate = False
+    logger.addHandler(handler)
+    return logger
