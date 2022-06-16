a1627ae85fa8d67c76ef9085e72aff38b34b61c7
Ronroeandassociates <3321@holbertonschool.com>
2022-06-16 17:17:58 -0500

    Perhaps I am overthinking it

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -5,12 +5,58 @@ Write a function called filter_datum that returns the log message obfuscated:
 
 import re
 import logging
+import os
+from typing import List
+import mysql.connector
 
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-"""
-1 Log formatter
-"""
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Returns a connector to a secure database
+    using server environmental vairables
+    """
+    env_username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    env_password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    env_hostName = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    env_dbName = os.environ.get("PERSONAL_DATA_DB_NAME")
+    connector = mysql.connector.connect(user=env_username,
+                                        password=env_password,
+                                        database=env_dbName,
+                                        host=env_hostName)
+    return connector
+
+
+def get_logger() -> logging.Logger:
+    """
+    2 Create logger
+    """
+
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    stream = logging.StreamHandler()
+    stream.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream)
+    return logger
+
+
+def filter_datum(fields: List[str], redaction: str, msg: str,
+                 separator: str) -> str:
+    """
+    fields: list of strings representing all fields to obfuscate
+    redaction: string representing by what the field will be obfuscated
+    message: string representing the log line
+    separator: string representing by which character is separating all
+    fields in the log line (message)
+    return: string representing the log line obfuscated
+    """
+    for fields in fields:
+        pattern = r'(?<={}=).*?(?={})'.format(fields, separator)
+        message = re.sub(pattern, redaction, message)
+    return message
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class"""
@@ -24,38 +70,9 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-
     def format(self, record: logging.LogRecord) -> str:
         ''' Format the log message '''
         msg = super().format(record)
-        logList =  (filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR))
+        logList = (filter_datum(self.fields, self.REDACTION,
+                                msg, self.SEPARATOR))
         return logList.replace(";", "; ")
-        NotImplementedError
-
-def filter_datum(fields, redaction, message, separator):
-    """
-    fields: list of strings representing all fields to obfuscate
-    redaction: string representing by what the field will be obfuscated
-    message: string representing the log line
-    separator: string representing by which character is separating all
-    fields in the log line (message)
-    return: string representing the log line obfuscated
-    """
-    for fields in fields:
-        regex = fr"(?<={fields}=).*?(?={separator})"
-        message = re.sub(r'{}'.format(regex), redaction, message)
-    return message
-
-def get_logger() -> logging.Logger:
-    """
-    2 Create logger
-    """
-
-    logger = logging.getLogger('user_data')
-    logger.setLevel(logging.INFO)
-    formatter = RedactingFormatter(PII_FIELDS)
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logger.propagate = False
-    logger.addHandler(handler)
-    return logger
