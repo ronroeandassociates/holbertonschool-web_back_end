4dfe004ec306bc3cbad60a530f66ecf8c7b74bbc
Ronroeandassociates <3321@holbertonschool.com>
2022-06-16 11:19:23 -0500

    let's edit it more

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """
-0 Regex-ing
 Write a function called filter_datum that returns the log message obfuscated:
 """
 
@@ -12,66 +11,61 @@ from os import environ
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 """
-1 Log formatter
+Log formatter
 """
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class"""
+    """Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: list):
-        ''' Initialize the formatter with the fields to redact'''
+    def __init__(self, fields: List[str]):
+        """Initialize RedactingFormatter class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = list(fields)
 
     def format(self, record: logging.LogRecord) -> str:
-        ''' Format the log message '''
+        """Format log record"""
         msg = super().format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str)->str:
-    """
-    fields: list of strings representing all fields to obfuscate
-    return: string representing the log line obfuscated
-    """
-    for fields in fields:
-        regexString = f"(?<={fields}=).*?(?={separator})"
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Filter using regex"""
+    for field in fields:
+        regexString = f"(?<={field}=).*?(?={separator})"
         message = re.sub(regexString, redaction, message)
     return message
 
 
 def get_logger() -> logging.Logger:
-    """
-    2 Create logger
-    """
+    """create logger"""
+    user_data = logging.getLogger("user_data")
 
-    logger = logging.getLogger('user_data')
-    logger.setLevel(logging.INFO)
-    formatter = RedactingFormatter(PII_FIELDS)
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logger.propagate = False
-    logger.addHandler(handler)
-    return logger
+    # set min to INFO
+    user_data.setLevel(logging.INFO)
+    user_data.propagate = False
+
+    # create handler and set format with our PII_FIELDS
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    # add handler to logger
+    user_data.addHandler(streamHandler)
+    return user_data
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    3 Create database connection
-    Connect to a secure holberton database to read a users table.
-    The database is protected by a username and password that are
-    stored in environment variables
-    """
+    """Gets and returns a database connection"""
     db = mysql.connector.connect(
-        db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-        db_passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-        db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-        database = os.getenv('PERSONAL_DATA_DB_NAME', 'personal_data')
+        host=environ.get("PERSONAL_DATA_DB_HOST"),
+        user=environ.get("PERSONAL_DATA_DB_USERNAME"),
+        password=environ.get("PERSONAL_DATA_DB_PASSWORD"),
+        database=environ.get("PERSONAL_DATA_DB_NAME")
+    )
     return db
 
 
@@ -80,14 +74,14 @@ def main():
     logger = get_logger()
     db = get_db()
 
-    # create cursor, execute queries in all rows
+    """ create cursor, execute queries in all rows"""
     cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM users;")
 
     for row in cursor:
-        # list of tuples
+        """ list of tuples"""
         tupleList = row.items()
-        # transform to list of strings with PII_FIELDS
+        """ transform to list of strings with PII_FIELDS """
         str = "; ".join(f"{tuple[0]}={tuple[1]}" for tuple in tupleList)
         logger.info(str)
 
