6b696c57755a90efc82fe15eb96da7e9cd8556cd
Ronroeandassociates <3321@holbertonschool.com>
2022-06-16 10:57:13 -0500

    Lets try that again

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -12,4 +12,4 @@ def _hash_password(password: str) -> str:
     """
     Hash a password for storing in a database.
     """
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
