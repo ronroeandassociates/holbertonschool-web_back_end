bf65b4a656fed274b0d3318407829b5a96b24306
Ronroeandassociates <3321@holbertonschool.com>
2022-06-16 11:06:01 -0500

    why oh why

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -11,3 +11,8 @@ def _hash_password(password: str) -> bytes:
     Hash a password for storing in a database.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Verifies a password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
