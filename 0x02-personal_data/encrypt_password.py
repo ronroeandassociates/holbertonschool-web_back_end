63cdd34925c613853669f6a829ccea74144eb241
Ronroeandassociates <3321@holbertonschool.com>
2022-06-16 10:54:59 -0500

    let's just see

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -5,3 +5,11 @@ User passwords should NEVER be stored in plain text in a database.
 Implement a hash_password function that expects one string argument name password and returns a salted, hashed password, which is a byte string.
 Use the bcrypt package to perform the hashing (with hashpw).
 """
+
+import bcrypt
+
+def _hash_password(password: str) -> str:
+    """
+    Hash a password for storing in a database.
+    """
+    return bcrypt.hashpw(password, bcrypt.gensalt())
