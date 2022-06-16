a22acf8ff86496da3f54b50c386d8332321f9594
Ronroeandassociates <3321@holbertonschool.com>
2022-06-16 10:53:31 -0500

    [FIX]clean up comments redo db and datasum

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -6,12 +6,15 @@ Write a function called filter_datum that returns the log message obfuscated:
 
 import re
 import logging
-
+from typing import list
+import mysql.connector
+from os import environ
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 """
 1 Log formatter
 """
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class"""
@@ -23,29 +26,24 @@ class RedactingFormatter(logging.Formatter):
     def __init__(self, fields: list):
         ''' Initialize the formatter with the fields to redact'''
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
+        self.fields = list(fields)
 
     def format(self, record: logging.LogRecord) -> str:
         ''' Format the log message '''
         msg = super().format(record)
-        logList =  (filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR))
-        return logList.replace(";", "; ")
-        NotImplementedError
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
 
-def filter_datum(fields, redaction, message, separator):
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str)->str:
     """
     fields: list of strings representing all fields to obfuscate
-    redaction: string representing by what the field will be obfuscated
-    message: string representing the log line
-    separator: string representing by which character is separating all
-    fields in the log line (message)
     return: string representing the log line obfuscated
     """
     for fields in fields:
-        regex = fr"(?<={fields}=).*?(?={separator})"
-        message = re.sub(r'{}'.format(regex), redaction, message)
+        regexString = f"(?<={fields}=).*?(?={separator})"
+        message = re.sub(regexString, redaction, message)
     return message
+
 
 def get_logger() -> logging.Logger:
     """
@@ -60,3 +58,41 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
     logger.addHandler(handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    3 Create database connection
+    Connect to a secure holberton database to read a users table.
+    The database is protected by a username and password that are
+    stored in environment variables
+    """
+    db = mysql.connector.connect(
+        db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+        db_passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+        db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+        database = os.getenv('PERSONAL_DATA_DB_NAME', 'personal_data')
+    return db
+
+
+def main():
+    """Connects to db, gets info, and displays it"""
+    logger = get_logger()
+    db = get_db()
+
+    # create cursor, execute queries in all rows
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users;")
+
+    for row in cursor:
+        # list of tuples
+        tupleList = row.items()
+        # transform to list of strings with PII_FIELDS
+        str = "; ".join(f"{tuple[0]}={tuple[1]}" for tuple in tupleList)
+        logger.info(str)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
