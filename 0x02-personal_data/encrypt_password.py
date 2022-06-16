0f2f50b40321225d55d94416b0d148b02ef71d7d
Ronroeandassociates <3321@holbertonschool.com>
2022-06-16 11:00:49 -0500

    Let's see if that works

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 """
 5 Encrypting passwords
-User passwords should NEVER be stored in plain text in a database.
-Implement a hash_password function that expects one string argument name password and returns a salted, hashed password, which is a byte string.
-Use the bcrypt package to perform the hashing (with hashpw).
 """
 
 import bcrypt
+
 
 def _hash_password(password: str) -> str:
     """
