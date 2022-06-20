7a6edcf8bc134f56c290f4ff253ff9296d9e8225
Ronroeandassociates <3321@holbertonschool.com>
2022-06-20 18:04:18 -0500

    TASK: 2 Error handler: Forbidden
    What the HTTP status code for a request where the user is authenticate but not allowed to access to a resource? 403 of course!
    
    Edit api/v1/app.py:
    
    Add a new error handler for this status code, the response must be:
    a JSON: {error: Forbidden}
    status code 403
    you must use jsonify from Flask
    For testing this new error handler, add a new endpoint in api/v1/views/index.py:
    
    Route: GET /api/v1/forbidden
    This endpoint must raise a 403 error by using abort - Custom Error Pages
    By calling abort(403), the error handler for 403 will be executed

--- 0x03-Basic_authentication/api/v1/app.py
+++ 0x03-Basic_authentication/api/v1/app.py
@@ -21,6 +21,13 @@ def error_unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+@app.errorhandler(403)
+def error_forbidden(error) -> str:
+    """
+    Error handler for 403 error
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
