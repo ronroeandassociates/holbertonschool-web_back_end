befb2f90c40c3fbf91d340e87671d0a1ae9edd46
Ronroeandassociates <3321@holbertonschool.com>
2022-06-20 17:56:09 -0500

    TASK: 1 Error handler: Unauthorized
    What the HTTP status code for a request unauthorized? 401 of course!
    
    Edit api/v1/app.py:
    
    Add a new error handler for this status code, the response must be:
    a JSON: {error: Unauthorized}
    status code 401
    you must use jsonify from Flask
    For testing this new error handler, add a new endpoint in api/v1/views/index.py:
    
    Route: GET /api/v1/unauthorized
    This endpoint must raise a 401 error by using abort - Custom Error Pages
    By calling abort(401), the error handler for 401 will be executed

--- 0x03-Basic_authentication/api/v1/app.py
+++ 0x03-Basic_authentication/api/v1/app.py
@@ -14,6 +14,14 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(401)
+def error_unauthorized(error) -> str:
+    """
+    Error handler for 401 error
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
