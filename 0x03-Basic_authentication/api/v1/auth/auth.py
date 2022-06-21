dc8e0f5eee2ae11219a08682a1ad7d32a0d020b9
Ronroeandassociates <3321@holbertonschool.com>
2022-06-20 19:49:45 -0500

    TASK: 4 Define which routes don't need authentication
    Update the method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: in Auth that returns True if the path is not in the list of strings excluded_paths:
    
    Returns True if path is None
    Returns True if excluded_paths is None or empty
    Returns False if path is in excluded_paths
    You can assume excluded_paths contains string path always ending by a /
    This method must be slash tolerant: path=/api/v1/status and path=/api/v1/status/ must be returned False if excluded_paths contains /api/v1/status/

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -6,25 +6,39 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth():
 
     def __init__(self):
         pass
 
-    def require_auth(self, path: str, excluded_paths: List[str] = []) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        require_auth public method
+        require_auth function
         """
-        if path in excluded_paths:
-            return False
+        if excluded_paths is None or excluded_paths == '':
+            return True
+        if path is not None:
+            if path[len(path) - 1] is not '/':
+                path += '/'
+        if path is None:
+            return True
+        for item in excluded_paths:
+            asterisk = item.find("*")
+            if asterisk != -1 and len(path) >= len(item):
+                pathcpy = path[: asterisk]
+                if pathcpy == item[: asterisk]:
+                    return False
+            elif path == item:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
 
         """authorization_header public method
     """
-    if request is None:
-        request = request
+        if request is None:
+            request = request
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current_user public method
