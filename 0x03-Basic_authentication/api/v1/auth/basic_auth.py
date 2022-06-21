104b4f40e9ed556119661e78d41db6c5e426daaa
Ronroeandassociates <3321@holbertonschool.com>
2022-06-21 16:06:56 -0500

    8. Basic - Base64 decode mandatory Add the method def decode_base64_authorization_header(self, base64_authorization_header: str) -> str: in the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header:
    Return None if base64_authorization_header is None
    Return None if base64_authorization_header is not a string
    Return None if base64_authorization_header is not a valid Base64 - you can use try/except
    Otherwise, return the decoded value as UTF8 string - you can use decode('utf-8')

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,18 +1,33 @@
+
 #!/usr/bin/env python3
-"""
-Module for basic authentication
-"""
+"""Module for basic authentication"""
+
 
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
-    """
-    Basic authentication class
-    """
-    def __init__(self) -> None:
-        """
-        Initialize the class
-        """
-        super().__init__()
+    """Basic Auth class"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts base64 authorization header"""
+        if authorization_header is not None:
+            if type(authorization_header) is str:
+                if authorization_header.startswith('Basic '):
+                    return authorization_header[6:]
+
+        return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Decodes base64 header"""
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+
+        except Exception:
+            return None
