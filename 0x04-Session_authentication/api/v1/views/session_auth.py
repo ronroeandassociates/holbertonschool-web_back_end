2a193e2aeae64f7c212125173bb3bd69f559887c
Ronroeandassociates <3321@holbertonschool.com>
2022-06-22 13:48:20 -0500

    Tasks 7 New view for Session Authentication
    Create a new Flask view that handles all routes for the Session authentication.
    
    In the file api/v1/views/session_auth.py, create a route POST /auth_session/login (= POST /api/v1/auth_session/login):
    
    Slash tolerant (/auth_session/login == /auth_session/login/)
    You must use request.form.get() to retrieve email and password parameters
    If email is missing or empty, return the JSON { error: email missing } with the status code 400
    If password is missing or empty, return the JSON { error: password missing } with the status code 400
    Retrieve the User instance based on the email - you must use the class method search of User (same as the one used for the BasicAuth)
    If no User found, return the JSON { error: no user found for this email } with the status code 404
    If the password is not the one of the User found, return the JSON { error: wrong password } with the status code 401 - you must use is_valid_password from the User instance
    Otherwise, create a Session ID for the User ID:
    You must use from api.v1.app import auth - WARNING: please import it only where you need it - not on top of the file (can generate circular import - and break first tasks of this project)
    You must use auth.create_session(..) for creating a Session ID
    Return the dictionary representation of the User - you must use to_json() method from User
    You must set the cookie to the response - you must use the value of the environment variable SESSION_NAME as cookie name - tip
    In the file api/v1/views/__init__.py, you must add this new view at the end of the file
    [NOT WORKING CORRECTLY geting Unathorized on all tests]

---
+++ 0x04-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""view of Session authentication"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route("/auth_session/login/",
+                 methods=["POST"], strict_slashes=False)
+def session_id():
+    """loggin user id"""
+    from api.v1.app import auth
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+
+    foundUsers = User.search({'email': email})
+    if not foundUsers:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in foundUsers:
+        valid_passwd = user.is_valid_password(password)
+    if valid_passwd is False:
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user.id)
+    SESSION_NAME = getenv("SESSION_NAME")
+    response = jsonify(user.to_json())
+    response.set_cookie(SESSION_NAME, session_id)
+
+    return response
