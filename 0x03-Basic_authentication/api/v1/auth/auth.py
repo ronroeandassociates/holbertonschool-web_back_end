d5ab75b5f547a7f3097be20ed2b26b53e0ecf7dd
Ronroeandassociates <3321@holbertonschool.com>
2022-06-21 20:16:56 -0500

    clean up more files and typos

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -1,49 +1,31 @@
 #!/usr/bin/env python3
-"""
- create a class Auth: to manage the API authentication
-"""
+"""Module for API authentication"""
+
 
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth():
-
-    def __init__(self):
-        pass
+    """API authentication class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        require_auth function
-        """
-        if excluded_paths is None or excluded_paths == '':
+        """Returns false if path is in excluded_paths"""
+        if path is None or excluded_paths is None:
             return True
-        if path is not None:
-            if path[len(path) - 1] is not '/':
-                path += '/'
-        if path is None:
-            return True
-        for item in excluded_paths:
-            asterisk = item.find("*")
-            if asterisk != -1 and len(path) >= len(item):
-                pathcpy = path[: asterisk]
-                if pathcpy == item[: asterisk]:
-                    return False
-            elif path == item:
-                return False
+
+        if path in excluded_paths or f"{path}/" in excluded_paths:
+            return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
-
-        """authorization_header public method
-    """
-        if request is None:
+        """Returns flask request or none if no header"""
+        if request is None or request.headers.get('Authorization') is None:
             return None
-        return request.headers.get("Authorization", None)
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """current_user public method
-        """
-        if request is None:
-            request = request
+        """Returns current user or none"""
         return None
