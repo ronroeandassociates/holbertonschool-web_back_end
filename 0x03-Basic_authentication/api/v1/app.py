211e2d56cc89786ffcd452f802e8b25a574bc937
Ronroeandassociates <3321@holbertonschool.com>
2022-06-21 20:13:09 -0500

    changing some simple typos

--- 0x03-Basic_authentication/api/v1/app.py
+++ 0x03-Basic_authentication/api/v1/app.py
@@ -6,37 +6,36 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-from api.v1.auth.auth import Auth
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
 auth = None
-if os.getenv("AUTH_TYPE") == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
+auth_type = getenv('AUTH_TYPE', 'basic')
+
+if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
-@app.before_request
-def request_filter() -> None:
-    """ Checks if request needs authorization
-    """
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-        ]
 
-    if auth and auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ 401 handler """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ 403 handler """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(404)
@@ -46,18 +45,20 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized request handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
+@app.before_request
+def before_request():
+    """Filters requests to correct error handlers"""
+    authList = ["/api/v1/status/",
+                "/api/v1/unauthorized",
+                "/api/v1/forbidden"]
 
+    if auth and auth.require_auth(request.path, authList):
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden request handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
