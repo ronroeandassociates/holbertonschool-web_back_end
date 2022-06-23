2848aecac90aaa84fd1090503df7ae0b2e904b07
Ronroeandassociates <3321@holbertonschool.com>
2022-06-23 13:42:13 -0500

    [FIXED] PYCODE STYLE FOR THE LOSS

--- 0x04-Session_authentication/api/v1/views/session_auth.py
+++ 0x04-Session_authentication/api/v1/views/session_auth.py
@@ -39,7 +39,9 @@ def login():
 
     return response
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def logout():
     """ DELETE /api/v1/auth_session/logout
     Return:
