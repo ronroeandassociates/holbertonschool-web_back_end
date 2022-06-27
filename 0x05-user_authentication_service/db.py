4dc3ff66641b7037305fa97fb1fece6afa2fe44c
Ronroeandassociates <3321@holbertonschool.com>
2022-06-27 12:41:16 -0500

    TASK 1 create user
    In this task, you will complete the DB class provided below to implement the add_user method
    
    Note that DB._session is a private property and hence should NEVER be used from outside the DB class
    
    Implement the add_user method, which has two required string arguments: email and hashed_password, and returns a User object. The method should save the user to the database. No validations are required at this stage

---
+++ 0x05-user_authentication_service/db.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
+from user import User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    """
+    Note that DB._session is a private property
+    and hence should NEVER be used from outside the DB class
+
+    Implement the add_user method, which has two required string arguments:
+    email and hashed_password, and returns a User object
+    The method should save the user to the database
+    No validations are required at this stage
+    """
+    def add_user(self, email, hashed_password: str) -> User:
+        """Add a new user to the database
+        """
+        user = User(email=email, hash_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
