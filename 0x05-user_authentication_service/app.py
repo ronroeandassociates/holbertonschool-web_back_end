f7309b5fc30f1004f486b0aa0590e637899b0d0b
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 18:27:30 -0500

    Task 6 Basic Flask app
    set up a basic Flask app.
    
    Create a Flask app that has a single GET route (/) and use flask.jsonify to return a JSON payload of the form:
    
    {message: Bienvenue}
    
    Add the following code at the end of the module:
    
    if __name__ == __main__:
        app.run(host=0.0.0.0, port=5000)

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+from flask import Flask, request, jsonify, abort
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /status
+    Return:
+      - JSON payload
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
