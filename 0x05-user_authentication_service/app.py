ceb64a81d138eb6efd6774235845cd958f43a294
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 18:40:31 -0500

    Task 7 Register user
    implement the end-point to register a user. Define a users function that implements the POST /users route.
    
    Import the Auth object and instantiate it at the root of the module as such:
    
    from auth import Auth
    
    AUTH = Auth()
    
    The end-point should expect two form data fields: email and password. If the user does not exist, the end-point should register it and respond with the following JSON payload:
    
    {email: <registered email>, message: user created}
    If the user is already registered, catch the exception and return a JSON payload of the form
    
    {message: email already registered}
    and return a 400 status code

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify, abort
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +14,26 @@ def status() -> str:
       - JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def new_user() -> str:
+    """If user no exist register user"""
+    # Get data from form request, change to request.get_json() for body
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+            }), 400
 
 
 if __name__ == "__main__":
