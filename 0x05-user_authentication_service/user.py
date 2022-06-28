5aaabd85f7e4489b1632bd1e47b4698f61bfc315
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 12:25:19 -0500

    [TYPO] added ed to make hashed

--- 0x05-user_authentication_service/user.py
+++ 0x05-user_authentication_service/user.py
@@ -25,6 +25,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hash_password = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
