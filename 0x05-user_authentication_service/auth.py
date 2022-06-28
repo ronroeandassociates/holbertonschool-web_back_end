f95ad533bbe8e45a95d98d0463412f1259fa58ee
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 18:13:29 -0500

    Task 5 Register user implement the Auth.register_user in the Auth class:
    from db import DB
    
    class Auth:
        Auth class to interact with the authentication database.
    
        def __init__(self):
            self._db = DB()
    
    Auth.register_user should take mandatory email and password string arguments and return a User object.
    
    If a user already exist with the passed email, raise a ValueError with the message User <user's email> already exists
    
    If not, hash the password with _hash_password, save the user to the database using self._db and return the User object

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -2,8 +2,28 @@
 """
 User model and authentication service
 """
+from atexit import register
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+       """Register a new user"""
+       try:
+        self.db.find_user_by(email=email)
+        raise ValueError(f"User{email} already exists")
+       except NoResultFound:
+        hashed_password = hashpw(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
 
 def _hash_password(password: str) -> bytes:
     """Hash a password"""
