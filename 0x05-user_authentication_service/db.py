9b5604b2e8e33084f232d2571df8a91b51ea83fd
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 17:05:30 -0500

    lets reword some stuff

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -39,10 +39,10 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
     """
     implement the DB.find_user_by method
     """
