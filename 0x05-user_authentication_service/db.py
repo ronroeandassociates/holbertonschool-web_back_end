76deec7648f6755cfc3583eef21034eae4f7c75b
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 17:43:05 -0500

    Task 3 update user
    implement the DB.update_user method that takes as argument a required user_id integer and arbitrary keyword arguments, and returns None.
    
    The method will use find_user_by to locate the user to update, then will update the user’s attributes as passed in the method’s arguments then commit changes to the database
    
    If an argument that does not correspond to a user attribute is passed, raise a ValueError

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -48,3 +48,15 @@ class DB:
             raise NoResultFound
         except InvalidRequestError:
             raise InvalidRequestError
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user by a given attribute"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+
+        self._session.commit()
+        return None
