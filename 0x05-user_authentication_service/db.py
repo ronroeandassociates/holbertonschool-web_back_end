4f974287c24e1b19db9f815da54ebb255dbbb7aa
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 17:11:05 -0500

    lets reword some stuff

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -6,8 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
 from user import Base, User
 
 
@@ -39,10 +40,10 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
+        return user
     """
     implement the DB.find_user_by method
     """
