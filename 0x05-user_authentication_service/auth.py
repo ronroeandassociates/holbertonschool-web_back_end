3b1b0c1f97d6ace4123a969f7b3ea18c372888ab
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 18:21:26 -0500

    [rework] register_user

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -8,6 +8,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -16,14 +17,15 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-       """Register a new user"""
-       try:
-        self.db.find_user_by(email=email)
-        raise ValueError(f"User{email} already exists")
-       except NoResultFound:
-        hashed_password = hashpw(password)
-        user = self._db.add_user(email, hashed_password)
-        return user
+        """ Registers a new user if email isn't listed"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password"""
