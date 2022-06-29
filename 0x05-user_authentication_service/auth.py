6874fd62e11428a3ecb80911cabfa2bf50b79037
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 21:34:08 -0500

    Task 18 Update password
    implement the Auth.update_password method It takes reset_token string argument and a password string argument and returns None
    
    Use the reset_token to find the corresponding user
    If it does not exist, raise a ValueError exception
    
    Otherwise, hash the password and update the user’s hashed_password field with the new hashed password and the reset_token field to None

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -74,6 +74,15 @@ class Auth:
         except Exception:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update user password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            pw = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=pw, reset_token=None)
+        except Exception:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password"""
