0db6e4b2ba12a25a6b5ddaa0d8b76c297e8ee25a
Ronroeandassociates <3321@holbertonschool.com>
2022-06-28 19:54:33 -0500

    Task 11 Log in
    implement a login function to respond to the POST /sessions route
    
    The request is expected to contain form data with email and a password fields.
    
    If the login information is incorrect, use flask.abort to respond with a 401 HTTP status.
    
    Otherwise, create a new session for the user, store it the session ID as a cookie with key session_id on the response and return a JSON payload of the form
    
    {email: <user email>, message: logged in}

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -36,5 +36,22 @@ def new_user() -> str:
             }), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """Create a new session"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not(Auth.valid_login(
+        email=email, password=password)
+           ) or not email or not password:
+        abort(401)
+
+    session_id = AUTH.create_session(email=email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
