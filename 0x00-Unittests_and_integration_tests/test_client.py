428c75c98c9a75be73525f87f48a1059b704ef28
Ronroeandassociates <3321@holbertonschool.com>
2022-07-06 14:41:54 -0500

    [TASK] 4 Parameterize and patch as decorators
    a new test_client.py file, declare the TestGithubOrgClient(unittest.TestCase) class and implement the test_org method
    
    Use @patch as a decorator to make sure get_json is called once with the expected argument but make sure it is not executed
    
    Use @parameterized.expand as a decorator to parametrize the test with a couple of org examples to pass to GithubOrgClient, in this order:
    
    google
    abc

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -2,3 +2,23 @@
 """_
 Parameterize a unit test
 """
+
+import unittest
+from unittest import patch, TestCase, MagicMock
+from client import GithubOrgClient
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test GithubOrgClient"""
+
+    @parameterized.expand([
+      ("google"),
+      ("abc")
+    ])
+    @patch("client.get_json",
+           MagicMock(return_value={"key": "value"}))
+    def test_org(self, org_name):
+        """Test org"""
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, {"key": "value"})
