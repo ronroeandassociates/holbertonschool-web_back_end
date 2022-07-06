32a4e9783c9a09fb07eeb38d83988fab7acaf069
Ronroeandassociates <3321@holbertonschool.com>
2022-07-06 12:42:09 -0500

    [TASK] 0 Parameterize a unit test
    write the first unit test for utils.access_nested_map.
    
    Create a TestAccessNestedMap class that inherits from unittest.TestCase
    
    Implement the TestAccessNestedMap.test_access_nested_map method to test that the method returns what it is supposed to
    
    Decorate the method with @parameterized.expand to test the function for following inputs:
    
    For each of these inputs, test with assertEqual that the function returns the expected result.
    
    The body of the test method should not be longer than 2 lines

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
-"""_
-Parameterize a unit test
-"""
+"""Module for test_utils"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ Define the TestAccessNestedMap Class """
+    """ Defines the TestAccessNestedMap"""
     @parameterized.expand([
-      ({"a": 1}, ("a",), 1),
-      ({"a": {"b": 2}}, ("a",), {'b': 2})
-      ({"a": {"b": 2}}, (("a", "b"), 2))
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def TestAccessNestedMap(self, nested_map, path, expected):
-        """
-        Test the access of a nested map
-        """
-        self.assertEqual(access_nested_map(nested_map), expected)
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test that utils.access_nested_map returns the expected result"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
