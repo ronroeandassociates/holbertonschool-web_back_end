bc5c8a8493f3b51f9026fd49359f070e39e57b12
Ronroeandassociates <3321@holbertonschool.com>
2022-07-06 14:17:23 -0500

    [TASK] 3 Parameterize and patch
    Implement the TestMemoize(unittest.TestCase) class with a test_memoize method.
    
    Use unittest.mock.patch to mock a_method.
    
    Test that when calling a_property twice, the correct result is returned but a_method is only called once using assert_called_once

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Module for test_utils"""
-import unittest
+
+from unittest import TestCase, mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
-from unittest.mock import TestCase, mock
+from utils import access_nested_map, get_json, memoize
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestAccessNestedMap(TestCase):
     """ Defines the TestAccessNestedMap"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -26,6 +26,7 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as exception_context_manager:
             access_nested_map(nested_map, path)
 
+
 class TestGetJson(TestCase):
     """Test get_json"""
 
@@ -38,3 +39,35 @@ class TestGetJson(TestCase):
         with mock.patch("requests.get") as mock_get:
             mock_get.return_value.json.return_value = payload
             self.assertEqual(get_json(url), payload)
+
+
+class TestMemoize(TestCase):
+    """Test memoize"""
+
+    def test_memoize(self):
+        """Test asserting memoize sets attribute"""
+
+        class TestClass:
+            """Test class for memoize"""
+
+            def a_method(self):
+                """Test a_method"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """test a_property"""
+                return self.a_method()
+
+        test_object = TestClass()
+        test_object.a_method = mock.MagicMock(return_value=42)
+        self.assertEqual(test_object.a_property, 42)
+        self.assertEqual(test_object.a_property, 42)
+        test_object.a_method.assert_called_once()
+
+        with mock.patch.object(TestClass, "a_method",
+                               return_value=42) as mock_method:
+            test_object2 = TestClass()
+            self.assertEqual(test_object2.a_property, 42)
+            self.assertEqual(test_object2.a_property, 42)
+            mock_method.assert_called_once()
