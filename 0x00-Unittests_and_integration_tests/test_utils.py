70e106d986c4699cb7a8f35d2a29511de0001e56
Ronroeandassociates <3321@holbertonschool.com>
2022-07-06 13:53:38 -0500

    [TASK] 1 Parameterize a unit test
    Implement TestAccessNestedMap.test_access_nested_map_exception. Use the assertRaises context manager to test that a KeyError is raised for the following inputs (use @parameterized.expand):
    
    Also make sure that the exception message is as expected.

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test that utils.access_nested_map returns the expected result"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that an exception is raised"""
+        with self.assertRaises(KeyError) as exception_context_manager:
+            access_nested_map(nested_map, path)
