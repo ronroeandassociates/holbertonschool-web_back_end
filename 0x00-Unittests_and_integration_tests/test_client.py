660958db2970f6c61091b7846195fb082813a2d8
Ronroeandassociates <3321@holbertonschool.com>
2022-07-06 14:47:16 -0500

    [TASK] 5 Mocking a property
    Implement the test_public_repos_url method to unit-test GithubOrgClient._public_repos_url
    
    Use patch as a context manager to patch GithubOrgClient.org and make it return a known payload
    
    Test that the result of _public_repos_url is the expected one based on the mocked payload

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -4,21 +4,29 @@ Parameterize a unit test
 """
 
 import unittest
-from unittest import patch, TestCase, MagicMock
+from unittest import patch, TestCase, MagicMock, mock, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
 
 
-class TestGithubOrgClient(unittest.TestCase):
-    """Test GithubOrgClient"""
+class TestGithubOrgClient(TestCase):
+    """Test for GithubOrgClient"""
 
     @parameterized.expand([
-      ("google"),
-      ("abc")
+        ("google"),
+        ("abc")
     ])
     @patch("client.get_json",
-           MagicMock(return_value={"key": "value"}))
+           MagicMock(return_value={'key': 'value'}))
     def test_org(self, org_name):
-        """Test org"""
+        "test org method"
         client = GithubOrgClient(org_name)
-        self.assertEqual(client.org, {"key": "value"})
+        self.assertEqual(client.org, {'key': 'value'})
+
+    def test_public_repos_url(self):
+        """Test public_repos_url method"""
+        with patch("client.get_json",
+                   new_callable=PropertyMock,
+                   return_value={"repos_url": "url"}):
+            client = GithubOrgClient("google")
+            self.assertEqual(client._public_repos_url, "url")
