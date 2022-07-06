32804851a7613072f267680109a5a237da899467
Ronroeandassociates <3321@holbertonschool.com>
2022-07-06 12:37:00 -0500

    [TASK] 0 Parameterize a unit test
    write the first unit test for utils.access_nested_map.
    
    Create a TestAccessNestedMap class that inherits from unittest.TestCase
    
    Implement the TestAccessNestedMap.test_access_nested_map method to test that the method returns what it is supposed to
    
    Decorate the method with @parameterized.expand to test the function for following inputs:
    
    For each of these inputs, test with assertEqual that the function returns the expected result.
    
    The body of the test method should not be longer than 2 lines

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -2,3 +2,19 @@
 """_
 Parameterize a unit test
 """
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Define the TestAccessNestedMap Class """
+    @parameterized.expand([
+      ({"a": 1}, ("a",), 1),
+      ({"a": {"b": 2}}, ("a",), {'b': 2})
+      ({"a": {"b": 2}}, (("a", "b"), 2))
+    ])
+    def TestAccessNestedMap(self, nested_map, path, expected):
+        """
+        Test the access of a nested map
+        """
+        self.assertEqual(access_nested_map(nested_map), expected)
