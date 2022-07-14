5d0a4cf1f36aaa0bfe827e5903e4b98756a3b0bc
Ronroeandassociates <3321@holbertonschool.com>
2022-07-14 11:57:24 -0500

    [TASK] 1 In and not out
    a SQL script that creates a table users following these requirements:
    
    id, integer, never null, auto increment and primary key
    
    email, string (255 characters), never null and unique
    
    name, string (255 characters)
    
    country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
    
    If the table already exists, your script should not fail
    
    Your script can be executed on any database

--- 0x01-MySQL_Advanced/1-country_users.sql
+++ 0x01-MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
   id INTEGER NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255),
-  country VARCHAR(255) NOT NULL DEFAULT 'US',
+  country ENUM('US', 'CO', 'TN') NOT NULL,
   PRIMARY KEY (id)
 );
 -- end of script
