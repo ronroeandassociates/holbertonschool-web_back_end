2294f273822374c6918e94ec205726c9c0ac5458
Ronroeandassociates <3321@holbertonschool.com>
2022-07-18 12:40:57 -0500

    change some syntax

--- 0x01-MySQL_Advanced/10-div.sql
+++ 0x01-MySQL_Advanced/10-div.sql
@@ -9,15 +9,15 @@
 DELIMITER |
 DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv(
-	a INT,
-	b INT)
+			a INT,
+			b INT)
 RETURNS FLOAT
 BEGIN
-	IF b = 0 THEN
-		RETURN 0;
-	ELSE
-		RETURN a / b;
-	END IF;
+			IF b = 0 THEN
+						RETURN 0;
+			ELSE
+						RETURN a / b;
+			END IF;
 END;
 |
 --end of script
