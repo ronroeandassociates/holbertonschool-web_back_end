5f16d8c5a05c65c003656c760f79c55fa72d3311
Ronroeandassociates <3321@holbertonschool.com>
2022-07-17 20:26:52 -0500

    [TASK] 2 Best band ever
    a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
    
    Import this table dump: metal_bands.sql.zip
    Column names must be: origin and nb_fans
    Your script can be executed on any database

---
+++ 0x01-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,9 @@
+-- a SQL script that ranks country origins of bands
+-- ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
+-- Your script can be executed on any database
+
+SELECT origin, SUM(fans) as nb_fans FROM 'metal_bands'
+GROUP BY 'origin'
+ORDER BY 'nb_fans' DESC;
+--end of script
