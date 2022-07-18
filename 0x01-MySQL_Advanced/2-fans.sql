3450832ea881d29a20c6acbb820b80593b2b97df
Ronroeandassociates <3321@holbertonschool.com>
2022-07-17 20:45:13 -0500

    changed back to select and simplified more

--- 0x01-MySQL_Advanced/2-fans.sql
+++ 0x01-MySQL_Advanced/2-fans.sql
@@ -3,8 +3,9 @@
 -- Column names must be: origin and nb_fans
 -- Your script can be executed on any database
 
-SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2020) - `formed` as `lifespan`
-  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-  ORDER BY `lifespan` DESC;
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
+
 --end of script
