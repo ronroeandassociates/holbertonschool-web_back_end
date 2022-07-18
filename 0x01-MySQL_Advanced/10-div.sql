2b1bc76bfcadbdca3d84de79bc17bf8ae5718b5a
Ronroeandassociates <3321@holbertonschool.com>
2022-07-18 14:46:15 -0500

    try rewriting the delimiter

--- 0x01-MySQL_Advanced/10-div.sql
+++ 0x01-MySQL_Advanced/10-div.sql
@@ -1,7 +1,20 @@
 --a SQL script that creates a function SafeDiv that divides (and returns) the first by the second
 -- number or returns 0 if the second number is equal to 0
 
-DELIMITER // CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT BEGIN IF b = 0 THEN RETURN 0; ELSE RETURN a / b; END IF; END // DELIMITER // DROP FUNCTION SafeDiv;
+DELIMITER //
 
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT)
+RETURNS FLOAT
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	ELSE
+		RETURN a / b;
+	END IF;
+END //
 
 DELIMITER ;
+
+--end of script
