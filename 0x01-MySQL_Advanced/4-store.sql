a96e080efe176cefa57446a708444299764f046a
Ronroeandassociates <3321@holbertonschool.com>
2022-07-17 20:54:15 -0500

    why do I try to complicate things

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -1,18 +1,12 @@
 --  SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
 -- Quantity in the table items can be negative
 
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS items;
+CREATE TRIGGER trigger_name
+AFTER INSERT
+ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
 
-
-CREATE TABLE IF NOT EXISTS orders (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  item_id INTEGER NOT NULL,
-  quantity INTEGER NOT NULL,
-);
-
-CREATE TABLE IF NOT EXISTS items (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  quantity INTEGER NOT NULL,
-);
+-- end of script
