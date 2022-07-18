9fbde671e82b2ec40207a12a3b87f6635abe55e6
Ronroeandassociates <3321@holbertonschool.com>
2022-07-18 15:10:32 -0500

    please say a space doesn't mess it up

--- 0x01-MySQL_Advanced/10-div.sql
+++ 0x01-MySQL_Advanced/10-div.sql
@@ -1,6 +1,4 @@
---a SQL script that creates a function SafeDiv that divides (and returns)
--- the first by the second
--- number or returns 0 if the second number is equal to 0
+-- a SQL script that creates a function SafeDiv that divides
 
 DELIMITER //
 
