c19422f380947d79dd6915bf0302bbfab4514beb
Ronroeandassociates <3321@holbertonschool.com>
2022-07-17 20:49:03 -0500

    simplify sometimes the ai makes it more difficult

--- 0x01-MySQL_Advanced/3-glam_rock.sql
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -3,7 +3,9 @@
 -- Column names must be: band_name and lifespan (in years)
 -- You should use attributes formed and split for computing the lifespan
 -- Your script can be executed on any database
-SELECT 'band_name' (IFNULL('split', '2022') - 'formed') AS 'lifespan'
-FROM metal_bands WHERE style LIKE '%glam rock'
-ORDER BY 'lifespan' DESC;
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
+
 --end of script
