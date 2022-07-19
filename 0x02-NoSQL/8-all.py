22dc1bb12f2bd18196d191c1ebf2661f3b17956b
Ronroeandassociates <3321@holbertonschool.com>
2022-07-19 18:56:12 -0500

    [Task] 8 List all documents in Python
    a Python function that lists all documents in a collection:
    
    Prototype: def list_all(mongo_collection):
    Return an empty list if no document in the collection
    mongo_collection will be the pymongo collection object

---
+++ 0x02-NoSQL/8-all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# 8 List all documents in Python
+# a Python function that lists all documents in a collection
+'''
+Prototype: def list_all(mongo_collection):
+Return an empty list if no document in the collection
+mongo_collection will be the pymongo collection object
+'''
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    '''
+    List all documents in a collection
+    '''
+    if mongo_collection is None:
+        return []
+    return mongo_collection.find()
