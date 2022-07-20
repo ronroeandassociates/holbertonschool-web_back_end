dd4781abb04a38da50e40b9b8f87d5a4656c8886
Ronroeandassociates <3321@holbertonschool.com>
2022-07-19 19:48:32 -0500

    [Task] 11 Where can I learn Python?
    a Python function that returns the list of school having a specific topic:
    
    Prototype: def schools_by_topic(mongo_collection, topic):
    mongo_collection will be the pymongo collection object
    topic (string) will be topic searched

---
+++ 0x02-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#11 Where can I learn Python?
+#a Python function that returns the list of school having a specific topic:
+'''
+Prototype: def schools_by_topic(mongo_collection, topic):
+mongo_collection will be the pymongo collection object
+topic (string) will be topic searched
+'''
+
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''returns the list of schools having a specific topic'''
+    if mongo_collection is None:
+        return []
+
+    return mongo_collection.find({'topics': topic})
