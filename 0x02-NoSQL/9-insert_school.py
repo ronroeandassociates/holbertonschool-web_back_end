b07cdcd1b99811210857dacac214c021135b25ae
Ronroeandassociates <3321@holbertonschool.com>
2022-07-19 19:15:20 -0500

    [Task] 9 Insert a document in Python
    a Python function that inserts a new document in a collection based on kwargs:
    
    Prototype: def insert_school(mongo_collection, **kwargs):
    mongo_collection will be the pymongo collection object
    Returns the new _id

---
+++ 0x02-NoSQL/9-insert_school.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# 9 Insert a document in Python
+# a Python function that inserts a new document in a collection based on kwargs:
+'''
+Prototype: def insert_school(mongo_collection, **kwargs):
+mongo_collection will be the pymongo collection object
+Returns the new _id
+'''
+
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''
+    Insert a new document in a collection
+    '''
+    if mongo_collection is None:
+        return None
+    return mongo_collection.insert_one(kwargs).inserted_id
