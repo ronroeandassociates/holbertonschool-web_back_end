035e071f43eec67d5c9eaf45df4dd9cfced1e55c
Ronroeandassociates <3321@holbertonschool.com>
2022-07-20 08:31:14 -0500

    massaged the print

--- 0x02-NoSQL/12-log_stats.py
+++ 0x02-NoSQL/12-log_stats.py
@@ -1,42 +1,38 @@
 #!/usr/bin/env python3
-# 12 Log stats
-# a Python script that provides some stats about Nginx logs stored in MongoDB:
-
+# a Python script that provides some stats about Nginx logs stored in MongoDB
 from pymongo import MongoClient
 
 
 def stats():
-    # first line: x logs where x is the number of documents in this collection
-    # connect to MongoDB
+    """
+    Count documents in collection with:
+    - Database: logs
+    - Collection: nginx
+    Print:
+    - Total number of documents in given collection
+    - Number of documents with method ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    - Number of documents with method=GET and path=/status
+    """
+    # Connect to MongoDB
     client = MongoClient()
-    # get the database
+    # Get logs database
     db = client.logs
-    # get Nginx collection from the database
+    # Get Nginx collection from logs database
     collection = db.nginx
 
     # Print number of total documents in the collection
     print(f"{collection.count_documents({})} logs")
 
-    '''
-    second line: Methods:
-    5 lines with the number of documents with the method =
-    ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order
-    (see example below - warning: it`s a tabulation before each line)
-    one line with the number of documents with:
-    method=GET
-    path=/status
-    '''
-
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    # Print Number of each method
+    # For each method, print number of documents with that method
     for method in methods:
-        print(f"{collection.count_documents({'method': method})} {method}")
-# TODO: FIX THIS format should be tab method  Method: xxx
-    # Print number of documents with  method=GET and path=/status
-    print(f"\t method {method}:" +
-          f"{collection.count_documents({'method': 'GET', 'path': '/status'})}\
-          GET /status")
+        print(f"\tmethod {method}: " +
+              f"{collection.count_documents({'method': method})}")
+
+    # Print number of documents with method=GET and path=/status
+    print(f"{collection.count_documents({'method': 'GET', 'path': '/status'})} \
+status check")
 
 
 if __name__ == "__main__":
