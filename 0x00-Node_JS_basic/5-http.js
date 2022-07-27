0ad9b684eeda6687d3a74d391c5d7ad3933e5adb
Ronroeandassociates <3321@holbertonschool.com>
2022-07-27 16:08:42 -0500

    [TASK] 5 Create a more complex HTTP server using Node's HTTP module
    In a file named 5-http.js, create a small HTTP server using the http module:
    
    It should be assigned to the variable app and this one must be exported
    HTTP server should listen on port 1245
    It should return plain text
    When the URL path is /, it should display Hello Holberton School! in the page body
    When the URL path is /students, it should display This is the list of our students followed by the same content as the file 3-read_file_async.js (with and without the database) - the name of the database must be passed as argument of the file
    CSV file can contain empty lines (at the end) - and they are not a valid student!

---
+++ 0x00-Node_JS_basic/5-http.js
@@ -0,0 +1,36 @@
+// 5 Create a more complex HTTP server using Node's HTTP module
+
+// In a file named 5-http.js, create a small HTTP server using the http module:
+// It should be assigned to the variable app and this one must be exported
+// HTTP server should listen on port 1245
+// It should return plain text
+// When the URL path is /, it should display Hello Holberton School! in the page body
+// When the URL path is /students, it should display This is the list of our students
+// followed by the same content as the file 3-read_file_async.js (with and without the database)
+// the name of the database must be passed as argument of the file
+// CSV file can contain empty lines (at the end) - and they are not a valid student!
+
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(process.argv[2]);
+      res.write(students.join('\n'));
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+}).listen(port);
+
+module.exports = app;
