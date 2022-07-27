9a2b8c418278bce2a82be7a194aa109b01ca1520
Ronroeandassociates <3321@holbertonschool.com>
2022-07-27 13:34:37 -0500

    trying to fix lint

--- 0x00-Node_JS_basic/2-read_file.js
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -1,7 +1,8 @@
 // function named countStudents. It should accept a path in argument
 // The script should attempt to read the database file synchronously
 // If the database is not available, it should throw an error with the text Cannot load the database
-// If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
+// If the database is available, it should log the following message to the console
+// Number of students: NUMBER_OF_STUDENTS
 // It should log the number of students in each field, and the list with the following format:
 // Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
 
