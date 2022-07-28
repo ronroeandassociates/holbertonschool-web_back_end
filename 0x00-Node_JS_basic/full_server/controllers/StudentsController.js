09d353f70650bb0f43e03788adbef7b0536b2838
Ronroeandassociates <3321@holbertonschool.com>
2022-07-27 21:30:43 -0500

    [TASK] 8.3 Write the Students controller Inside the file full_server/controllers/StudentsController.js, create a class named StudentsController. Add two static methods:
    The first one is getAllStudents:
    
    The method accepts request and response as argument
    It should return a status 200
    It calls the function readDatabase from the utils file, and display in the page:
    First line: This is the list of our students
    And for each field (order by alphabetic order case insensitive), a line that displays the number of students in the field, and the list of first names (ordered by appearance in the database file) with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
    If the database is not available, it should return a status 500 and the error message Cannot load the database
    
    The second one is getAllStudentsByMajor:
    
    The method accepts request and response as argument
    It should return a status 200
    It uses a parameter that the user can pass to the browser major. The major can only be CS or SWE. If the user is passing another parameter, the server should return a 500 and the error Major parameter must be CS or SWE
    It calls the function readDatabase from the utils file, and display in the page the list of first names for the students (ordered by appearance in the database file) in the specified field List: LIST_OF_FIRSTNAMES_IN_THE_FIELD
    If the database is not available, it should return a status 500 and the error message Cannot load the database

---
+++ 0x00-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,59 @@
+// create a class named StudentsController
+
+import readDatabase from '../utils';
+
+class StudentsController {
+  //first create a static method named getStudents
+  static getStudents(_req, res) {
+    res.status(200);
+    readDatabase('process.argv[2]')
+      .then((data) => {
+        res.write('This is the list of our students\n');
+        const fields = Object.keys(data);
+
+        for (let i = 0; i < fields.length; i += 1) {
+          res.write(
+            `Number of students in ${fields[i]}: ${
+              data[fields[i]].numStudents
+            }. `,
+          );
+          res.write(`List: ${data[fields[i]].names.join(', ')}`);
+
+          if (i < fields.length - 1) {
+            res.write('\n');
+          }
+        }
+      })
+      .catch((err) => {
+        res.write(err.message);
+      })
+      .finally(() => {
+        res.end();
+      });
+  }}
+
+  static getAllStudentsByMajor(req, res) {
+    res.status(200);
+    readDatabase(process.argv[2])
+      .then((data) => {
+        const fields = Object.keys(data);
+
+        const { major } = req.params;
+
+        if (fields.includes(major)) {
+          res.write(`List: ${data[major].names.join(', ')}`);
+        } else {
+          res.status(500);
+          res.write('Major parameter must be CS or SWE');
+        }
+      })
+      .catch((err) => {
+        res.write(err.message);
+      })
+      .finally(() => {
+        res.end();
+      });
+  }
+}
+
+module.exports = StudentsController;
