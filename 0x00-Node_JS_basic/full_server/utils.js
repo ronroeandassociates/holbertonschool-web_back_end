cddd59a994d77f260b683575d936ec9a3d793a86
Ronroeandassociates <3321@holbertonschool.com>
2022-07-27 20:58:17 -0500

    [TASK] 8.1 Organize the structure of the server Create 2 directories within: controllers routes Create a file full_server/utils.js, in the file create a function named readDatabase that accepts a file path as argument: It should read the database asynchronously It should return a promise When the file is not accessible, it should reject the promise with the error When the file can be read, it should return an object of arrays of the firstname of students per fields

---
+++ 0x00-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,10 @@
+// Create a file full_server/utils.js, in the file create a function named
+//readDatabase that accepts a file path
+
+import countStudents from '../3-read_file_async';
+
+// read the database asynchronously
+
+const readDatabase = (path) => countStudents(path);
+
+module.exports = readDatabase;
