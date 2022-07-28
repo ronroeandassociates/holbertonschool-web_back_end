533f947427331af0395a28d6f736439b5b978e1a
Ronroeandassociates <3321@holbertonschool.com>
2022-07-27 19:53:35 -0500

    [TASK] 6. Create a small HTTP server using Express mandatory Install Express and in a file named 6-http_express.js, create a small HTTP server using Express module:
    It should be assigned to the variable app and this one must be exported
    HTTP server should listen on port 1245
    Displays Hello Holberton School! in the page body for the endpoint /

---
+++ 0x00-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+// create a small HTTP server using Express module
+
+const express = require('express');
+
+const port = 1245;
+
+const app = express();
+
+app.get('/', (_req, res) => res.send('Hello Holberton School!'))
+.listen(port);
+
+module.exports = app;
