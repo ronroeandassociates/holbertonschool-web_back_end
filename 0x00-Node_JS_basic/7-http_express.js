ccbd6dad81fd9c0b1ddf13b3c7018932f183bb26
Ronroeandassociates <3321@holbertonschool.com>
2022-07-27 20:38:12 -0500

    Fix

--- 0x00-Node_JS_basic/7-http_express.js
+++ 0x00-Node_JS_basic/7-http_express.js
@@ -18,28 +18,14 @@ app.get('/', (_req, res) => {
 });
 
 app.get('/students', async (_req, res) => {
-  res.write('This is the list of our students\n');
-  await countStudents(process.argv[2])
-    .then((data) => {
-      const fields = Object.keys(data);
-      const total = fields.reduce(
-        (acc, curr) => acc + data[curr].NUMBER_OF_STUDENTS,
-        0,
-      );
-      res.write(`Total number of students: ${total}\n`);
-      for (let i = 0; i < fields.length; i += 1) {
-        res.write(
-          `${fields[i]}: ${data[fields[i]].NUMBER_OF_STUDENTS}\n`);
-      }
-      res.write('List of students:\n');
-    })
-
-
-      .catch((err) => {
-        res.write(err.message);
-      });
+  const message = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(process.argv[2]);
+    res.send(`${message}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${message}${error.message}`);
+  }
 });
-
 app.listen(port);
 
 module.exports = app;
