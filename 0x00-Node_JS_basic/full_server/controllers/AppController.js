ed16fdbdbb2f6c9b34edef1a5495c80d71fc8e5f
Ronroeandassociates <3321@holbertonschool.com>
2022-07-27 21:03:02 -0500

    [TASK] 8.2 Write the App controller Inside the file full_server/controllers/AppController.js:
    Create a class named AppController. Add a static method named getHomepage
    The method accepts request and response as argument. It returns a 200 status and the message Hello Holberton School!

---
+++ 0x00-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,11 @@
+// Create a class named AppController. Add a static method named getHomepage
+// The method accepts request and response as argument.
+// It returns a 200 status and the message Hello Holberton School!
+
+class AppController {
+  static getHomepage(_req, res) {
+    return res.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
