af4c7d61f746026ba8f065803f55f6d00bf32019
Ronroeandassociates <3321@holbertonschool.com>
2022-07-31 17:13:49 -0500

    🔧 build(basic test with mocha): 0 Basic test with M
    Install Mocha using npm:  Set up a scripts in your package.json to quickly run Mocha using npm test You have to use assert Create a new file named 0-calcul.js:  Create a function named calculateNumber. It should accepts two arguments (number) a and b The function should round a and b and return the sum of it Test cases  Create a file 0-calcul.test.js that contains test cases of this function You can assume a and b are always number Tests should be around the “rounded” part Tips:  For the sake of the example, this test suite is slightly extreme and probably not needed However, remember that your tests should not only verify what a function is supposed to do, but also the edge cases Requirements:  You have to use assert You should be able to run the test suite using npm test 0-calcul.test.js Every test should pass without any warning

---
+++ 0x01-unittests_in_js/0-calcul.js
@@ -0,0 +1,10 @@
+// Create a function named calculateNumber
+// It should accepts two arguments (number) a and b
+
+const calculateNumber = (a, b) => {
+
+  // Round a and b and return sum
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
---
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,21 @@
+// Create a file 0-calcul.test.js that contains test cases of this function
+// You can assume a and b are always number
+// Tests should be around the “rounded” part
+
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe ('Test suite', () => {
+  // Tests for calculateNumber function using Node.js assert
+  it('Test that calculateNumber sums two rounded numbers', () => {
+    assert.equal(calculateNumber(1, 2), 3);
+    assert.equal(calculateNumber(1.1, 2.1), 3);
+    assert.equal(calculateNumber(1.4, 2.4), 3);
+    assert.equal(calculateNumber(1.9, 2.9), 5);
+    assert.equal(calculateNumber(1.5, .5), 3);
+    assert.equal(calculateNumber(2, 0), 2);
+    assert.equal(calculateNumber(-7, 2.1), -5);
+    assert.equal(calculateNumber(8.4, -3), 5);
+    assert.equal(calculateNumber(-10.9, -3.6), -15);
+  });
+})
