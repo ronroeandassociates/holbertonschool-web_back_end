4f80b24e5b7cb07f222d92fcbcbd3d41fd68e330
Ronroeandassociates <3321@holbertonschool.com>
2022-08-02 22:42:51 -0500

    task 4

---
+++ 0x01-unittests_in_js/4-payment.test.js
@@ -0,0 +1,14 @@
+const sendPaymentRequestToAPI = require('./4-payment');
+const Utils = require('./utils');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('Test suite', () => {
+  it('Test that sendPaymentRequestToAPI sums two rounded numbers', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToAPI(1, 2);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 1, 2)).to.be.true;
+    spy.restore();
+  })
+})
