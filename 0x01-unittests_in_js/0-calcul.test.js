0a89c9223d092871cc66bc443eac024295131745
Ronroeandassociates <3321@holbertonschool.com>
2022-08-02 19:08:14 -0500

    🐞 fix:
    try to do it right

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@
 // Tests should be around the “rounded” part
 
 const assert = require('assert');
-const calculateNumber = require('./0-calcul').strict;
+const calculateNumber = require('./0-calcul');
 
 describe ('Test suite', () => {
   // Tests for calculateNumber function using Node.js assert
@@ -17,5 +17,10 @@ describe ('Test suite', () => {
     assert.equal(calculateNumber(-7, 2.1), -5);
     assert.equal(calculateNumber(8.4, -3), 5);
     assert.equal(calculateNumber(-10.9, -3.6), -15);
+    assert.equal(calculateNumber(0, 0), 0);
+    assert.equal(calculateNumber(-1, -1), -2);
+    assert.equal(calculateNumber(0, -1), -1);
+    assert.equal(calculateNumber(-1, 0), -1);
+    assert.equal(calculateNumber(-8, 1), -7);
   });
 })
